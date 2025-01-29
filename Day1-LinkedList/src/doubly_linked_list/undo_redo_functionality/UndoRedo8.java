26a7cf5d3bcfbf5f62e841e9d00ba2ef8a899383
Anush Tenguriya <anushtenguriya93100@gmail.com>
2025-01-29 12:30:10 +0530

    implementing undo redo functionalities using double-linked-list

---
+++ Day1-LinkedList/src/doubly_linked_list/undo_redo_functionality/UndoRedo8.java
@@ -0,0 +1,27 @@
+package doubly_linked_list.undo_redo_functionality;
+
+public class UndoRedo8 {
+    public static void main(String[] args) {
+        TextEditor editor = new TextEditor(10);
+
+        editor.addState("State 1: Hello");
+        editor.addState("State 2: Hello, World");
+        editor.addState("State 3: Hello, World!");
+        editor.displayCurrentState();
+
+        editor.undo();
+        editor.displayCurrentState();
+
+        editor.undo();
+        editor.displayCurrentState();
+
+        editor.redo();
+        editor.displayCurrentState();
+
+        editor.addState("State 4: Hello, World Again!");
+        editor.displayCurrentState();
+
+        editor.undo();
+        editor.displayCurrentState();
+    }
+}
