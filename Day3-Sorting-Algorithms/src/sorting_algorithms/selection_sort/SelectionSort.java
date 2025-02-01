a9b6670b8e17461b91b958605be7118e7be6f827
Anush Tenguriya <154823559+Anush93100@users.noreply.github.com>
2025-02-01 15:13:57 +0530

    Update SelectionSort.java

--- Day3-Sorting-Algorithms/src/sorting_algorithms/selection_sort/SelectionSort.java
+++ Day3-Sorting-Algorithms/src/sorting_algorithms/selection_sort/SelectionSort.java
@@ -6,7 +6,7 @@ public class SelectionSort {
         int n=arr.length;
         for(int i=0;i<n-1;i++){
             int minIndex=i;
-            for(int j=0;j<n;j++){
+            for(int j=i+1;j<n;j++){
                 if(arr[j]<arr[minIndex]){
                     minIndex=j;
                 }
